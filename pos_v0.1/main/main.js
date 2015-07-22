d1f7ff2226edf4c9bdc4c6243c2e4f629a62b627
anlin-yang <anlin0414@outlook.com>
2015-07-22 21:02:51 +0800

    对pos_v0,和pos_v0.1进行了重构，优化。

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,20 +1,31 @@
 function printReceipt(inputs) {
-    var result = '***<没钱赚商店>收据***\n';
-    var sum = 0;
-
-    for(var i = 0; i < inputs.length; i=i+count){   
-	var count = 0;
-	for(var j = 0; j < inputs.length; j++) {
-	    if(inputs[i].name == inputs[j].name) {
-	      count++;
-	    }
-	  }
-	sum += inputs[i].price * count;
-        result += '名称：' + inputs[i].name + '，数量：' + count + inputs[i].unit + '，单价：'+ 
-        inputs[i].price.toFixed(2) + '(元)，小计：' +   (count*inputs[i].price).toFixed(2) +'(元)\n';
-
-    }
-    result += '----------------------\n总计：' + sum.toFixed(2) + '(元)\n**********************';
-    console.log(result);
+  var tempInputs = [];
+  inputs.forEach(function(val) {
+    groupingItem(val, tempInputs);
+  });
+  printBills(tempInputs);
 }
 
+function groupingItem(obj, result) {
+  var existItems = result.filter(function(item) {
+    return item.barcode === obj.barcode;
+  });
+  if (existItems.length === 0) {
+    obj["count"] = 1;
+    result.push(obj);
+  } else {
+    existItems[0]["count"] += 1;
+  }
+}
+
+function printBills(inputs) {
+  var result = '***<没钱赚商店>收据***\n';
+  var sum = 0;
+  inputs.forEach(function(val) {
+    sum += val.price * val.count;
+    result += '名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' +
+      val.price.toFixed(2) + '(元)，小计：' + (val.count * val.price).toFixed(2) + '(元)\n';
+  });
+  result += '----------------------\n总计：' + sum.toFixed(2) + '(元)\n**********************';
+  console.log(result);
+}
--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,14 +1,11 @@
 function printReceipt(inputs) {
-    var result = '***<没钱赚商店>收据***\n';
-    var sum = 0;
-
-    for(var i = 0; i < inputs.length; i++){
-        sum += inputs[i].price * inputs[i].count;
-        result += '名称：' + inputs[i].name + '，数量：' + inputs[i].count + inputs[i].unit + '，单价：'+ 
-        inputs[i].price.toFixed(2) + '(元)，小计：' +   (inputs[i].count*inputs[i].price).toFixed(2) +'(元)\n';
-
-    }
-    result += '----------------------\n总计：' + sum.toFixed(2) + '(元)\n**********************';
-    console.log(result);
+  var result = '***<没钱赚商店>收据***\n';
+  var sum = 0;
+  inputs.forEach(function(val) {
+    sum += val.price * val.count;
+    result += '名称：' + val.name + '，数量：' + val.count + val.unit + '，单价：' +
+      val.price.toFixed(2) + '(元)，小计：' + (val.count * val.price).toFixed(2) + '(元)\n';
+  });
+  result += '----------------------\n总计：' + sum.toFixed(2) + '(元)\n**********************';
+  console.log(result);
 }
-
