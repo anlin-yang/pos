28ff0d6bb45827846d73e866907ffa64219f0bd8
anlin-yang <anlin0414@outlook.com>
2015-07-26 09:16:30 +0800

    Finished pos_v2.

--- pos_v2/main/main.js
+++ pos_v2/main/main.js
@@ -1,1 +1,7 @@
-//TODO: Please write code in this file.
+function printReceipt(inputs) {
+  var scaner = new Scaner();
+  var cart = new Cart();
+  var pos = new Pos();
+  scaner.scan(inputs, cart);
+  pos.printBills(cart);
+}
